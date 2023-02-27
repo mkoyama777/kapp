6fa6bc058b4468c4daa18f1fa5ca7247b801f8c3
mkoyama777 <mk.expfac.uj@gmail.com>
2023-02-27 22:18:32 +0900

    dummy

--- application/controller/yolo.sh
+++ application/controller/yolo.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 pwd
-cd /opt/render/project/src/application/yoloface/
 echo $1
 echo $2
 echo $3
 echo $4
-python /opt/render/project/src/application/yoloface/yoloafce.py $1 $2 $3 $4
+python yoloface.py $1 $2 $3 $4
