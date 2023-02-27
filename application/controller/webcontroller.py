ce4d6eb260cf005bce1f1af319a99b3e4cc0d444
mkoyama777 <mk.expfac.uj@gmail.com>
2023-02-27 18:39:59 +0900

    dummy

--- application/controller/webcontroller.py
+++ application/controller/webcontroller.py
@@ -81,7 +81,12 @@ def upload(request,session,upload_dir,output_dir):
                 outputfilename = changesuffix(filename)
                 # th = threading.Thread(target=yolo.analyze,args=[get_filetype(filename),inputfilepath,outputdir,outputfilename])
                 # subprocess.Popen("python ../yoloface/yoloface.py",[get_filetype(filename),inputfilepath,outputdir,outputfilename])
-                cmd = ["controller/yolo.sh",get_filetype(filename),inputfilepath,outputdir,outputfilename]
+                cmd = ["pwd"]
+                subprocess.Popen(cmd,bufsize=1, shell=True)
+
+                cmd = ["./controller/yolo.sh",get_filetype(filename),inputfilepath,outputdir,outputfilename]
+                subprocess.Popen(cmd,bufsize=1, shell=True)
+                cmd = ["./yolo.sh",get_filetype(filename),inputfilepath,outputdir,outputfilename]
                 subprocess.Popen(cmd,bufsize=1, shell=True)
                 # th.start()
                 
