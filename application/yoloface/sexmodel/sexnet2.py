b1967843184b3531fb0a7e169845ab2215a7e102
mkoyama777 <mk.expfac.uj@gmail.com>
2023-03-24 18:43:32 +0900

    dummy

--- application/yoloface/sexmodel/sexnet2.py
+++ application/yoloface/sexmodel/sexnet2.py
@@ -91,7 +91,7 @@ def predict(img):
     parent_dir = os.path.abspath(parent_dir)
     print("sexweight:"+parent_dir)
     net = Net().cpu().eval()
-    net.load_state_dict(torch.load(parent_dir+os.sep+'sex_case_yolocut.pt', map_location=torch.device('cpu')))
+    net.load_state_dict(torch.load(parent_dir+os.sep+'sex_case5_yolocut.pt', map_location=torch.device('cpu')))
     img = Image.fromarray(img)
     # image = Image.open(img).convert("RGB")
     data = transform(img)
